7ed575e5f6057a854dfff52005a889e05fd9257e
kekw4u <dmitriiii95@mail.ru>
2024-07-25 20:49:17 +0300

    Добавил 2 парсера

--- map_parser_json.py
+++ map_parser_json.py
@@ -1,3 +1,4 @@
+#Доделать надо
 import json
 from time import sleep
 from selenium import webdriver
@@ -5,11 +6,11 @@ from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
 from selenium.webdriver.common.by import By
 
-RESPONSE_WAITING_TIME = 5
-SCROLL_PAUSE_TIME = 1
-DEFAULT_HEIGHT = 2000
-
 def map_parser(url, city, district, corner):
+    RESPONSE_WAITING_TIME = 5
+    SCROLL_PAUSE_TIME = 1
+    DEFAULT_HEIGHT = 2000
+
     #Настройка webdriver'а
     service = webdriver.ChromeService(executable_path="/usr/bin/chromedriver")
 
@@ -22,7 +23,7 @@ def map_parser(url, city, district, corner):
     driver = webdriver.Chrome(service = service, options=options)
     driver.set_window_size(1920, 1080)
     driver.get(url)
-    #Поиск строки для адреса
+
     search_bar = driver.find_element(By.CLASS_NAME, "input__control")
     search_bar.send_keys(city+' ', Keys.ENTER)
     sleep(RESPONSE_WAITING_TIME)
@@ -32,20 +33,17 @@ def map_parser(url, city, district, corner):
     #Перемещаем мышку к списку магазинов и листаем вниз, пока не появится div с классом add-business-view
     element = driver.find_element(By.CLASS_NAME, 'search-list-view__content')
     scroll_origin = ScrollOrigin.from_element(element)
-    d_y = 2000
 
     while True:
-        ActionChains(driver).scroll_from_origin(scroll_origin, 0, d_y).perform()
-        sleep(1)
+        ActionChains(driver).scroll_from_origin(scroll_origin, 0, DEFAULT_HEIGHT).perform()
+        sleep(SCROLL_PAUSE_TIME)
         try:
             driver.find_element(By.CLASS_NAME, "add-business-view")
-            d_y += 2000
+            DEFAULT_HEIGHT += 2000
             break
         except:
             continue
 
-    #Дальше есть два варианта - вся информация уже получена. Выбор: обрабатывать сам скролл бар и все элементы в нём или получать json-response, который приходит при прогрузке страницы и парсить его
-    #Я выбрал 2 вариант и решил парсить логи, т.к. в них больше информации
     def processLog(log):
         log = json.loads(log["message"])["message"]
         try:
@@ -63,6 +61,7 @@ def map_parser(url, city, district, corner):
 
     #Получаем логи
     logs = driver.get_log("performance")
+    print(logs)
 
     #Получаем все ответы
     responses = [processLog(log) for log in logs if processLog(log) != None]
@@ -72,10 +71,7 @@ def map_parser(url, city, district, corner):
             items.append(entry['data']['items'])
         except:
             continue
-    #В принципе, в items лежит вся нужная нам информация
-    print(items)
-    # Закрытие драйвера
-    sleep(10)
+
     driver.quit()
 
-map_parser("https://yandex.ru/maps/",'Алейск ', '',' Аникс')
+map_parser("https://yandex.ru/maps/",'Минск ', '','Гостиница')
