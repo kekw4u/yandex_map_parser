8853a2760b28e0535dd36a2ae32abdb8e4b49967
Anton <108623357+Tuwka12@users.noreply.github.com>
2024-07-26 10:48:48 +0300

    Сделал рефакторинг кода

--- map_parser_json.py
+++ map_parser_json.py
@@ -58,57 +58,57 @@ class YandexMapParser:
             except:
                 continue
 
-        def processLog(log):
+        def processLog(log: dict) -> dict:
             log_text = log["message"]
             log_json = json.loads(log["message"])["message"]
             try:
                 # TODO: Фильтровать ответы, наверное, можно лучше
                 if ("api/search" in log_text):
                     try:
-                        body = driver.execute_cdp_cmd('Network.getResponseBody',
-                                                      {'requestId': log_json["params"]["requestId"]})
-                        body = json.loads(body['body'])
-                        if "totalResultCount" in body['data']:
-                            return body
+                        request_id = log_json['params']['requestId']
+                        body = driver.execute_cdp_cmd('Network.getResponseBody', {'requestId': request_id})
+                        body_dict = json.loads(body['body'])
+                        if "totalResultCount" in body_dict['data']:
+                            return body_dict
                     except:
                         return
             except:
                 return
 
         logs = driver.get_log("performance")
-
         responses = [processLog(log) for log in logs if processLog(log) != None]
         return responses
 
-    def __parse_responses(self):
-        data = ''
+    def __parse_responses(self) -> list[dict]:
+        data = []
         responses = self.__get_responses()
         for response in responses:
             for item in response['data']['items']:
-                title = item['title']
-                address = item['address']
-                try:
-                    rating = item['ratingData']['ratingValue']
-                except:
-                    rating = ''
-                try:
-                    phone = item['phones'][0]['number']
-                except:
-                    phone = ''
-                try:
-                    url = item['urls'][0]
-                except:
-                    url = ''
-                res_str = f'{title};{address};{str(rating)};{phone};{url};\n'
-                if item['type'] == 'business' and res_str not in data:
-                    data += res_str
+                shop = {}
+                item_keys = item.keys()
+                shop['title'] = item['title']
+                shop['address'] = item['address']
+                if 'ratingData' in item_keys:
+                    shop['rating'] = item['ratingData']
+                if 'phones' in item_keys:
+                    shop['phones'] = item['phones']
+                if 'urls' in item_keys:
+                    shop['urls'] = item['urls']
+                if 'metro' in item_keys:
+                    item['nearest_metro_stations'] = []
+                    for metro_station in item['metro']:
+                        metro_station_dict = {''}
+                        item['nearset_metro_stations'].append()
+
+                if item['type'] == 'business' and shop not in data:
+                    data.append(shop)
         return data
 
     def upload_data(self) -> None:
         data = self.__parse_responses()
-        filename = f'data/{self.city} {self.district} {self.shop}.csv'
+        filename = f'data/{self.city} {self.district} {self.shop}.json'
         with open(filename, mode='w', encoding='utf-8') as fp:
-            fp.write(data)
+            fp.write(json.dumps(data))
 
 
 if __name__ == '__main__':
