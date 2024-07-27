37f550d357711f3dddfac85c50ba247a65ffa6a6
kekw4u <dmitriiii95@mail.ru>
2024-07-27 23:00:09 +0300

    Поправил файл requirements.txt, рефакторинг кода, исправил ошибку, когда существует всего один магазин в заданном диапазоне

--- YandexMapParserJson.py
+++ YandexMapParserJson.py
@@ -1,5 +1,7 @@
 import json
 from typing import List, Any
+
+import selenium.common.exceptions
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
@@ -10,24 +12,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from time import sleep
 
+
 class YandexMapParser:
     URL = 'https://yandex.ru/maps/'
     RESPONSE_WAITING_TIME = 15
     SCROLL_PAUSE_TIME = 1
-    CLICK_WAITING_TIME = 0.5
+    CLICK_WAITING_TIME = 1
     DEFAULT_HEIGHT = 2000
-
 
     SEARCH_BAR_CONDITIONS = (By.CLASS_NAME, "input__control")
     SIDE_PANEL_CONDITIONS = (By.CLASS_NAME, "search-list-view__content")
     SEARCH_BUTTON_CONDITIONS = (By.XPATH, "//button[@type='submit' and @aria-haspopup='false']")
-
+    ONE_SHOP_CARD_CLASS = "business-card-view__main-wrapper"
+    END_OF_LIST_CLASS = "add-business-view"
 
     def __init__(self, city: str, district: str, shop: str) -> None:
         self.city = city
         self.district = district
         self.shop = shop
-
 
     def __chrome_options(self) -> webdriver.ChromeOptions:
         # service = webdriver.ChromeService(executable_path="/usr/bin/chromedriver")
@@ -39,7 +41,6 @@ class YandexMapParser:
         options.add_argument('--window-size=1920,1080')
         options.set_capability('goog:loggingPrefs', {'performance': 'ALL', 'browser': 'ALL'})
         return options
-
 
     def __get_responses(self) -> list[Any | None]:
 
@@ -53,7 +54,15 @@ class YandexMapParser:
 
         self.__insert_querry(search_bar, wait)
 
-        side_panel = wait.until(EC.visibility_of_element_located(self.SIDE_PANEL_CONDITIONS))
+        try:
+            driver.find_element(By.CLASS_NAME, self.ONE_SHOP_CARD_CLASS)
+            logs = driver.get_log("performance")
+            responses = [YandexMapParser.__process_log(log, driver) for log in logs
+                         if YandexMapParser.__process_log(log, driver) is not None]
+            return responses
+        except selenium.common.exceptions.NoSuchElementException:
+            side_panel = wait.until(EC.visibility_of_element_located(self.SIDE_PANEL_CONDITIONS))
+
         scroll_origin = ScrollOrigin.from_element(side_panel)
         total_height = self.DEFAULT_HEIGHT
 
@@ -61,24 +70,22 @@ class YandexMapParser:
             ActionChains(driver).scroll_from_origin(scroll_origin, 0, total_height).perform()
             sleep(self.SCROLL_PAUSE_TIME)
             try:
-                driver.find_element(By.CLASS_NAME, "add-business-view")
+                driver.find_element(By.CLASS_NAME, self.END_OF_LIST_CLASS)
                 total_height += self.DEFAULT_HEIGHT
                 break
             except:
                 continue
 
         logs = driver.get_log("performance")
-        responses = [YandexMapParser.__process_log(log, driver) for log in logs 
-                     if YandexMapParser.__process_log(log, driver) != None]
+        responses = [YandexMapParser.__process_log(log, driver) for log in logs
+                     if YandexMapParser.__process_log(log, driver) is not None]
         return responses
-
 
     def __insert_querry(self, search_bar: WebElement, wait: WebDriverWait) -> None:
         for part in [f'{self.city} {self.district}', f' {self.shop}']:
             search_bar.send_keys(part, Keys.ENTER)
             sleep(self.CLICK_WAITING_TIME)
             wait.until(EC.visibility_of_element_located(self.SEARCH_BUTTON_CONDITIONS))
-
 
     @staticmethod
     def __process_log(log: dict, driver: webdriver) -> dict:
@@ -97,7 +104,6 @@ class YandexMapParser:
         except:
             return
 
-
     def __parse_responses(self) -> list[dict]:
         data = []
         responses = self.__get_responses()
@@ -106,9 +112,9 @@ class YandexMapParser:
             for item in response['data']['items']:
                 shop = {}
                 item_keys = item.keys()
-                params = ['title', 'address', 'ratingData', 'phones', 
+                params = ['title', 'address', 'ratingData', 'phones',
                           'urls', 'workingTimeText', 'socialLinks']
-                
+
                 for param in params:
                     if param in item_keys:
                         shop[param] = item[param]
@@ -130,7 +136,6 @@ class YandexMapParser:
                 if item['type'] == 'business' and shop not in data:
                     data.append(shop)
         return data
-
 
     def upload_data(self) -> None:
         data = self.__parse_responses()
