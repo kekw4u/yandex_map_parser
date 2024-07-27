b69ec757451e5d2051d69c8d0a4994683b67fa35
Anton <108623357+Tuwka12@users.noreply.github.com>
2024-07-27 17:28:35 +0300

    Рефакторинг кода

--- YandexMapParserJson.py
+++ YandexMapParserJson.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from time import sleep
 
+
 class YandexMapParser:
 
     URL = 'https://yandex.ru/maps/'
@@ -21,7 +22,8 @@ class YandexMapParser:
 
     SEARCH_BAR_CONDITIONS = (By.CLASS_NAME, "input__control")
     SIDE_PANEL_CONDITIONS = (By.CLASS_NAME, "search-list-view__content")
-    SEARCH_BUTTON_CONDITIONS = (By.XPATH, "//button[@type='submit' and @aria-haspopup='false']")
+    SEARCH_BUTTON_CONDITIONS = (
+        By.XPATH, "//button[@type='submit' and @aria-haspopup='false']")
 
 
     def __init__(self, cities: list, districts: list, shops: list) -> None:
@@ -39,7 +41,8 @@ class YandexMapParser:
         options.add_argument('--enable-logging')
         options.add_argument('--log-level=0')
         options.add_argument('--window-size=1920,1080')
-        options.set_capability('goog:loggingPrefs', {'performance': 'ALL', 'browser': 'ALL'})
+        options.set_capability('goog:loggingPrefs', 
+                               {'performance': 'ALL', 'browser': 'ALL'})
         return options
 
 
@@ -53,19 +56,23 @@ class YandexMapParser:
 
         wait = WebDriverWait(
             driver, YandexMapParser.RESPONSE_WAITING_TIME, poll_frequency=0.5)
-        
-        search_bar = wait.until(EC.visibility_of_element_located(
-            YandexMapParser.SEARCH_BAR_CONDITIONS))
+
+        search_bar = wait.until(
+            EC.visibility_of_element_located(
+                YandexMapParser.SEARCH_BAR_CONDITIONS))
 
         YandexMapParser.__insert_querry(search_bar, querry, wait)
 
-        side_panel = wait.until(EC.visibility_of_element_located(
-            YandexMapParser.SIDE_PANEL_CONDITIONS))
+        side_panel = wait.until(
+            EC.visibility_of_element_located(
+                YandexMapParser.SIDE_PANEL_CONDITIONS))
+
         scroll_origin = ScrollOrigin.from_element(side_panel)
         total_height = YandexMapParser.DEFAULT_HEIGHT
 
         while True:
-            ActionChains(driver).scroll_from_origin(scroll_origin, 0, total_height).perform()
+            ActionChains(driver).scroll_from_origin(
+                scroll_origin, 0, total_height).perform()
             sleep(YandexMapParser.SCROLL_PAUSE_TIME)
             try:
                 driver.find_element(By.CLASS_NAME, "add-business-view")
@@ -75,13 +82,13 @@ class YandexMapParser:
                 continue
 
         logs = driver.get_log("performance")
-        responses = [YandexMapParser.__process_log(log, driver) for log in logs 
+        responses = [YandexMapParser.__process_log(log, driver) for log in logs
                      if YandexMapParser.__process_log(log, driver) != None]
         return responses
 
 
     @staticmethod
-    def __insert_querry(search_bar: WebElement, 
+    def __insert_querry(search_bar: WebElement,
                         querry: list[str], wait: WebDriverWait) -> None:
         city, district, shop = querry
         for part in [f'{city} {district}', f' {shop}']:
@@ -89,8 +96,9 @@ class YandexMapParser:
 
             sleep(YandexMapParser.CLICK_WAITING_TIME)
 
-            wait.until(EC.visibility_of_element_located(
-                YandexMapParser.SEARCH_BUTTON_CONDITIONS))
+            wait.until(
+                EC.visibility_of_element_located(
+                    YandexMapParser.SEARCH_BUTTON_CONDITIONS))
 
 
     @staticmethod
@@ -101,7 +109,7 @@ class YandexMapParser:
             if ("api/search" in log_text):
                 try:
                     request_id = log_json['params']['requestId']
-                    body = driver.execute_cdp_cmd('Network.getResponseBody', 
+                    body = driver.execute_cdp_cmd('Network.getResponseBody',
                                                   {'requestId': request_id})
                     body_dict = json.loads(body['body'])
                     if "totalResultCount" in body_dict['data']:
@@ -121,9 +129,9 @@ class YandexMapParser:
             for item in response['data']['items']:
                 shop = {}
                 item_keys = item.keys()
-                params = ['title', 'address', 'ratingData', 'phones', 
+                params = ['title', 'address', 'ratingData', 'phones',
                           'urls', 'workingTimeText', 'socialLinks']
-                
+
                 for param in params:
                     if param in item_keys:
                         shop[param] = item[param]
@@ -131,15 +139,18 @@ class YandexMapParser:
                 if 'metro' in item_keys:
                     shop['nearest_metro_stations'] = []
                     for metro_station in item['metro']:
-                        metro_station_dict = {'station_name': metro_station['name'],
-                                              'station_distance': metro_station['distanceValue']}
-                        shop['nearest_metro_stations'].append(metro_station_dict)
+                        metro_station_dict = {
+                            'station_name': metro_station['name'], 
+                            'station_distance': metro_station['distanceValue']}
+                        shop['nearest_metro_stations'].append(
+                            metro_station_dict)
 
                 if 'stops' in item_keys:
                     shop['nearest_bus_stops'] = []
                     for bus_stop in item['stops']:
-                        bus_stop_dict = {'bus_stop_name': bus_stop['name'],
-                                         'bus_stop_distance': bus_stop['distanceValue']}
+                        bus_stop_dict = {
+                            'bus_stop_name': bus_stop['name'], 
+                            'bus_stop_distance': bus_stop['distanceValue']}
                         shop['nearest_bus_stops'].append(bus_stop_dict)
 
                 if item['type'] == 'business' and shop not in data:
@@ -155,7 +166,7 @@ class YandexMapParser:
         with open(filename, mode='w', encoding='utf-8') as fp:
             fp.write(json.dumps(data))
 
-    
+
     def upload_all_data(self) -> None:
         for city in self.cities:
             for district in self.districts:
